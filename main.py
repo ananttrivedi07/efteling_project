fa52665e49f730f113ef748ec2bdf22a02557726
ananttrivedi07 <anant.trivedi07@gmail.com>
2026-04-10 16:16:49 +0200

    added base_directory in load config for correctly pointing to config file.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from data_processing.functions import *
 import yaml
-
+import os
 CONFIG_file = "config.yaml"
 
 def load_config(config_file):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(base_dir, config_file)
     
-    with open(config_file, 'r') as file:
+    with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
     return config
 
