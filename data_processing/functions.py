cc808b5b6e1fc98d487ba65b6b4b2e38e1615efe
ananttrivedi07 <anant.trivedi07@gmail.com>
2026-04-10 15:49:58 +0200

    Extracting data from trashnet and showing 5 random images per class.

---
+++ data_processing/functions.py
@@ -0,0 +1,63 @@
+from glob import glob
+import os
+import matplotlib.pyplot as plt
+import random
+from PIL import Image
+import kagglehub
+import shutil
+
+
+def show_random_images_per_class(base_path, num_images=5):
+    """
+    Displays random images per class from a dataset folder.
+    """
+    
+    base_path = os.path.join(base_path, "dataset-resized")
+    classes = [cls for cls in os.listdir(base_path) 
+               if os.path.isdir(os.path.join(base_path, cls))]
+    
+    for cls in classes:
+        class_path = os.path.join(base_path, cls)
+        
+        # Only keep valid image files
+        images = [f for f in os.listdir(class_path)
+                  if f.lower().endswith(('.jpg'))]
+        
+        if len(images) == 0:
+            print(f"No images found in class {cls}")
+            continue
+        
+        sampled_images = random.sample(images, min(num_images, len(images)))
+        
+        plt.figure(figsize=(15, 3))
+        plt.suptitle(f"Class: {cls}", fontsize=14)
+        
+        for i, img_name in enumerate(sampled_images):
+            img_path = os.path.join(class_path, img_name)
+            
+            try:
+                img = Image.open(img_path)
+                
+                plt.subplot(1, num_images, i + 1)
+                plt.imshow(img)
+                plt.axis('off')
+            except Exception as e:
+                print(f"Skipping {img_path}: {e}")
+        
+        plt.show()
+
+    
+def run_data_processing(root_folder, create_plots=False):   
+    """
+    Get data from trashnet dataset, copy to local folder, and optionally create plots of random images per class.
+    """ 
+    path = kagglehub.dataset_download("feyzazkefe/trashnet")
+
+    target_dir = os.path.join(os.getcwd(), root_folder)
+    os.makedirs(target_dir, exist_ok=True)
+
+    shutil.copytree(path, target_dir, dirs_exist_ok=True)
+
+    print("Dataset copied to:", target_dir)
+    if create_plots:
+        show_random_images_per_class(target_dir)    
---
+++ main.py
@@ -0,0 +1,31 @@
+from data_processing.functions import *
+import yaml
+
+CONFIG_file = "config.yaml"
+
+def load_config(config_file):
+    
+    with open(config_file, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+def run_pipeline(config_file=None):
+    config = load_config(config_file)
+
+    # 1. data preprocessing
+    if config['flags']['processing_files']:
+        run_data_processing(root_folder="data", create_plots=True)
+    
+    # 2. feature extraction
+    
+    # 3. run model
+    
+    # 4. save results
+    print("Data pipeline completed successfully!")
+    
+
+if __name__ == "__main__":
+    print("Welcome to the Efteling project!")
+    run_pipeline(config_file=CONFIG_file)
+    
+    
