23881e8a0117ceafdbd1f7af567ca14475611f45
ananttrivedi07 <anant.trivedi07@gmail.com>
2026-04-15 17:44:25 +0200

    A better generalizable model, takes into consideration the color of the image better.

--- feature_processing/functions.py
+++ feature_processing/functions.py
@@ -89,13 +89,13 @@ def run_feature_processing(root_folder="data", show_training_data=False, show_ra
         transforms.RandomRotation(15),
         transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5]*3, std=[0.5]*3)
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
     val_test_transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5]*3, std=[0.5]*3)
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
     
     train_ds = ImageDataset(X_train, y_train, transform=train_transform)
